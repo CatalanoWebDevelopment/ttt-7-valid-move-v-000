5f710b99f5a9445294cb3cd219fd0a0bb0c74135
Parker Catalano <catalano.work@gmail.com>
2017-10-01 18:03:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == true
     return nil
-  elsif position_taken? == nil
+  elsif position_taken?(board, index) == nil
     return nil
   else
     return true
